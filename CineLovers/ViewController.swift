ecbc3da10374b851402c621621a634b4db0e1a82
Iago Maracajá <iago079@gmail.com>
2019-04-24 19:21:18 -0300

    Initial Commit

---
+++ CineLovers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CineLovers
+//
+//  Created by Iago Albuquerque on 24/04/19.
+//  Copyright © 2019 Andre Iago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
